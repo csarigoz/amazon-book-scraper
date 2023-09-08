c7d29cdb15afd33315a99d12cf6125fded009f04
Cagri Sarigoz <csarigoz@users.noreply.github.com>
2023-09-08 03:16:18 +0300

    Export both jsonl files to csv

--- export_csv.py
+++ export_csv.py
@@ -2,21 +2,25 @@ import json
 import csv
 
 # Open the input and output files
-with open('search_results_output.jsonl', 'r') as jsonl_file, open('output.csv', 'w', newline='') as csv_file:
-    # Create a CSV writer object
-    csv_writer = csv.writer(csv_file)
-    
-    # Initialize header
-    header_written = False
-    
-    for line in jsonl_file:
-        # Parse the JSON line to get a dictionary
-        data = json.loads(line)
+def export_jsonl_to_csv(infile, outfile):
+    with open(infile, 'r') as jsonl_file, open(outfile, 'w', newline='') as csv_file:
+        # Create a CSV writer object
+        csv_writer = csv.writer(csv_file)
         
-        # Write header if not done already
-        if not header_written:
-            csv_writer.writerow(data.keys())
-            header_written = True
+        # Initialize header
+        header_written = False
         
-        # Write the actual data
-        csv_writer.writerow(data.values())
+        for line in jsonl_file:
+            # Parse the JSON line to get a dictionary
+            data = json.loads(line)
+            
+            # Write header if not done already
+            if not header_written:
+                csv_writer.writerow(data.keys())
+                header_written = True
+            
+            # Write the actual data
+            csv_writer.writerow(data.values())
+
+export_jsonl_to_csv("output.jsonl", "output.csv")
+export_jsonl_to_csv("search_results_output.jsonl", "search_results_output.csv")
